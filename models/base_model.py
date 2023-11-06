1207445e0b76b2e4e1f15a293e6cabd75b88a448
CoderYounes <younesanas2021@gmail.com>
2023-11-06 21:51:59 +0100

    reformatting some unused things in the Code & change the f to format()

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         classname = self.__class__.__name__
-        return f"[{classname}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = str(datetime.datetime.now())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.created_at.isoformat()
 
