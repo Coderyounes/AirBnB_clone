e4476ba425b749c68ca4f1055332a73a52e7aa68
CoderYounes <younesanas2021@gmail.com>
2023-11-07 20:43:38 +0100

    The Console initial form

--- console.py
+++ console.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, line):
+        """Quit command to exit the program\n"""
+        return True
+
+    def do_EOF(self, line):
+        """exit the program after reach/get the end-of-file\n"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
