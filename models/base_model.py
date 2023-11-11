7e5db9a7768c9a5b61deaa43fc25ed2e77539420
escaper <assaoui54@gmail.com>
2023-11-11 11:12:45 +0100

    add save o storage if it was init

--- models/base_model.py
+++ models/base_model.py
@@ -17,14 +17,14 @@ class BaseModel:
                 if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.strptime(value, dformat))
                 elif key == '__class__':
-                    # dead code
-                    pass
+                    continue
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         classname = self.__class__.__name__
@@ -32,7 +32,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.new(self)
+        # models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
