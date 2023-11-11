8fc08b7b76e1dd024842383dc07340e9b52d0df2
escaper <assaoui54@gmail.com>
2023-11-11 16:49:42 +0100

    little fix for storage

--- tests/test_models/test_engines/test_file_storage.py
+++ tests/test_models/test_engines/test_file_storage.py
@@ -12,20 +12,24 @@ import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 import models
+import os
 
 
 class FileStorageTests(unittest.TestCase):
     """ all File Storage Tests """
 
-    my_model = BaseModel()
-
-    def test__objects(self):
-            self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+    def test_type_objects(self):
+            """ test __objects type """
+            self.assertEqual(type(models.storage.all()), dict)
+            # self.assertEqual(dict, type(FileStorage._FileStorage__objects))
 
     def test_all(self):
-            self.assertEqual(dict, type(models.storage.all()))
+            """ test all function """
+            self.assertEqual(type(models.storage.all()), dict)
+            # self.assertEqual(dict, type(models.storage.all()))
 
     def test_new(self):
+            """ test new functions """
             base_model = BaseModel()
             models.storage.new(base_model)
             models.storage.new(base_model)
@@ -33,12 +37,14 @@ class FileStorageTests(unittest.TestCase):
                           models.storage.all().keys())
 
     def test_save(self):
+            """ test save functions """
             self.my_model.save()
             self.assertEqual(
                 os.path.exists(models.storage._FileStorage__file_path),
                 True)
 
     def test_reload(self):
+            """ test reload function """
             base_model = BaseModel()
             models.storage.new(base_model)
             models.storage.save()
@@ -47,9 +53,11 @@ class FileStorageTests(unittest.TestCase):
             self.assertIn("BaseModel." + base_model.id, objs)
 
     def test_init(self):
+            """ test __init__ function """
             self.assertIsInstance(models.storage, FileStorage)
 
     def test_save(self):
+            """ test save with an arg """
             with self.assertRaises(TypeError):
                 models.storage.save(None)
 
